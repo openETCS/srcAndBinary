faf83bac9e26025f56f12fe00e59591419a7fc7e
Stefan Karg <info@stefankarg-it.de>
2015-11-09 12:38:05 +0100

    Imported code added

--- Green openETCS Non-Vital Demonstrator/Source Code/Source Code ETCS Onboard Unit System/Generated_Code/KCG-ERSA/RemoteDMIBus_EnvSim.h
+++ Green openETCS Non-Vital Demonstrator/Source Code/Source Code ETCS Onboard Unit System/Generated_Code/KCG-ERSA/RemoteDMIBus_EnvSim.h
@@ -1,6 +1,6 @@
-/* $*************** KCG Version 6.1.3 (build i6) ****************
-** Command: s2c613 -config R:/Repositories/modeling/model/Scade/System/OBU_PreIntegrations/EVC_IP_DMI/KCG_ERSA\kcg_s2c_config.txt
-** Generation date: 2015-11-09T11:52:23
+/* $**************** KCG Version 6.4 (build i21) ****************
+** Command: kcg64.exe -config S:/modeling/test/EnvSim/Scade/Simulation/config.txt
+** Generation date: 2015-10-19T20:52:26
 *************************************************************$ */
 #ifndef _RemoteDMIBus_EnvSim_H_
 #define _RemoteDMIBus_EnvSim_H_
@@ -8,6 +8,8 @@
 #include "kcg_types.h"
 
 /* =====================  no input structure  ====================== */
+
+/* =====================  no output structure  ====================== */
 
 /* ========================  context type  ========================= */
 typedef struct {
@@ -19,15 +21,19 @@ typedef struct {
 /* ===========  node initialization and cycle functions  =========== */
 /* EnvSim::RemoteDMIBus */
 extern void RemoteDMIBus_EnvSim(
-  /* EnvSim::RemoteDMIBus::evcToDMI */EVC_to_DMI_Message_int_T_API_DMI_Pkg *evcToDMI,
-  /* EnvSim::RemoteDMIBus::tiuToDMI */TIU_Input_msg_API_TIU_Pkg *tiuToDMI,
+  /* EnvSim::RemoteDMIBus::evcToDMI */ EVC_to_DMI_Message_int_T_API_DMI_Pkg *evcToDMI,
+  /* EnvSim::RemoteDMIBus::tiuToDMI */ TIU_Input_msg_API_TIU_Pkg *tiuToDMI,
   outC_RemoteDMIBus_EnvSim *outC);
 
+#ifndef KCG_NO_EXTERN_CALL_TO_RESET
 extern void RemoteDMIBus_reset_EnvSim(outC_RemoteDMIBus_EnvSim *outC);
+#endif /* KCG_NO_EXTERN_CALL_TO_RESET */
+
+extern void RemoteDMIBus_init_EnvSim(outC_RemoteDMIBus_EnvSim *outC);
 
 #endif /* _RemoteDMIBus_EnvSim_H_ */
-/* $*************** KCG Version 6.1.3 (build i6) ****************
+/* $**************** KCG Version 6.4 (build i21) ****************
 ** RemoteDMIBus_EnvSim.dh
-** Generation date: 2015-11-09T11:52:23
+** Generation date: 2015-10-19T20:52:26
 *************************************************************$ */
 
